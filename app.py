2077d3c35c6e27861dded6ac12a761396807c9ec
krishpatel143x-netizen <krishpatel143x@gmail.com>
2025-10-22 20:12:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ st.sidebar.header("Settings")
 model_choice = st.sidebar.selectbox(
     "Select Groq model (suggested)",
     options=[
-        "gpt-oss-120B",
+        "openai/gpt-oss-120b",
         "mixtral-8x7b-32768",
         "llama3-8b",
         "compound-beta",
